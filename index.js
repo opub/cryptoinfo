dcd969e448ce4dbaf90e997b158749e10b3fe3c6
Ted O'Connor <ted@opub.com>
2021-08-12 23:10:18 -0400

    switched to unmarshal api

--- index.js
+++ index.js
@@ -1,39 +1,47 @@
-const cheerio = require('cheerio');
+const config = require('config');
 const got = require('got');
 
-const domain = 'https://polygonscan.com/';
-const pages = new Set();
-const transactions = new Set();
+const API = 'https://api.unmarshal.com/v1';
+const USD = new Intl.NumberFormat('en-US', {
+    style: 'currency',
+    currency: 'USD'
+});
 
-async function getPage(href) {
-    if (!pages.has(href)) {
-        pages.add(href);
-        console.log('loading page', href);
+async function loadWallets() {
 
-        const response = await got(domain + href);
-        const body = cheerio.load(response.body);
+    const protocols = config.get('protocols');
+    const wallets = config.get('wallets');
+    const key = config.get('api_key');
+    let total = 0;
 
-        for (let link of body('a')) {
-            const href = link.attribs.href;
-            if (href && href.startsWith('/tx/')) {
-                transactions.add(href.substring(4));
-            }
-            else if (href && href.startsWith('txs?')) {
-                await getPage(href);
+    for (let p of protocols) {
+        for (let w of wallets) {
+            total += await getWallet(p, w, key);
+        }
+    }
+
+    console.log('TOTAL', USD.format(total));
+}
+
+async function getWallet(protocol, wallet, key) {
+    const url = `${API}/${protocol}/address/${wallet}/assets?auth_key=${key}`;
+    let total = 0;
+    try {
+        const response = await got(url);
+        const data = JSON.parse(response.body);
+        for (let token of data) {
+            total += token.quote;
+            if (token.quote > 0.005) {
+                console.log(protocol, wallet, token.contract_ticker_symbol, USD.format(token.quote));
             }
         }
     }
-}
-
-function getTrxn(href) {
-    console.log(href);
+    catch (e) {
+        console.log('failed', e);
+    }
+    return total;
 }
 
 (async function () {
-    await getPage('txs?a=0x4b223cb2e1f91ad59b86f5798a1fc53f73e244b0&p=1');
-    let i = 0;
-    for (let item of transactions) console.log(i++, 'trxn', item);
+    await loadWallets();
 })();
-
-
-// 2021-08-04 9:43am approved FUND to add liquidity
